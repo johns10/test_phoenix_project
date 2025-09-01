5e2303ad7281720bda9ae80fca5624e9bae30a47
John Davenport <johns10@gmail.com>
2025-09-01 07:31:27 -0400

    auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :test_phoenix_project, :scopes,
+  user: [
+    default: true,
+    module: TestPhoenixProject.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: TestPhoenixProject.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :test_phoenix_project,
   ecto_repos: [TestPhoenixProject.Repo],
   generators: [timestamp_type: :utc_datetime]
