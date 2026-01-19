1157154b4cec800b43563503259b0b884ecec02a
John Davenport <johns10@gmail.com>
2026-01-19 09:26:11 -0500

    deps, log level

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,7 @@
+case System.get_env("LOG_LEVEL") do
+  nil -> :ok
+  level -> Logger.configure(level: String.to_existing_atom(level))
+end
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(TestPhoenixProject.Repo, :manual)
