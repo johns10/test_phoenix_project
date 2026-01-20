9dbfe725fb120018179b5abc389948003246ffd5
John Davenport <johns10@gmail.com>
2026-01-20 10:15:58 -0500

    use hex for client utils

--- mix.exs
+++ mix.exs
@@ -67,11 +67,11 @@ defmodule TestPhoenixProject.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:client_utils, git: "https://github.com/Code-My-Spec/client_utils", branch: "main"},
+      {:client_utils, "~> 0.1.1"},
       {:mix_machine, git: "https://github.com/johns10/mix_machine", branch: "master"},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7.13", only: [:dev, :test], runtime: false},
-      {:sobelow, "~> 0.14", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.14", only: [:dev, :test], runtime: false}
     ]
   end
 
